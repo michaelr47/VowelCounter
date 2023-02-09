a38a2709caa0933a857312a9d2c5c7949dff7bcf
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-02-08 21:19:00 -0800

    feat modal with vowel count

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const input = document.getElementById('inputField');
 const checkBtn = document.getElementById('checkButton');
-const h1 = document.createElement('h1');
-
+const modal = document.getElementById('modal');
+const modalText = document.getElementById('vowelText');
+const h2 = document.createElement('h2');
+const closeModal = document.getElementById('closeModal');
 checkBtn.addEventListener('click', () => {
     if (!input.value) {
         input.placeholder = 'Please type here';   
     }
+    modal.style.display = 'block';
     
     let vowels = 'aeiouAEIOU';
     let valueArray = input.value.split('');
@@ -19,12 +22,19 @@ checkBtn.addEventListener('click', () => {
         
     }
     
-    h1.innerText = '';
-    h1.innerText = `There ${counter > 1 ? 'are' : 'is'} ${counter} ${counter > 1 ? 'vowels' : 'vowel'}!`;
-    if (counter === 0) h1.innerText = 'no vowels found';
-    document.body.append(h1);
+    h2.innerText = '';
+    h2.innerText = `There ${counter > 1 ? 'are' : 'is'} ${counter} ${counter > 1 ? 'vowels' : 'vowel'}!`;
+    if (counter === 0) h2.innerText = 'no vowels found';
+    modalText.append(h2);
     clearInputField();
+    closeModalContent(modal);
 })
+
+const closeModalContent = (closeModal) => {
+    closeModal.addEventListener('click', () => {
+        modal.style.display = 'none';
+    }) 
+}
 
 const clearInputField = () => { 
     input.value = '';
