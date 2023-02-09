d53091ddae10dfc87664ea1ac6adf94ae0509c23
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-02-08 19:19:08 -0800

    vowel count working

--- script.js
+++ script.js
@@ -2,14 +2,30 @@ const input = document.getElementById('inputField');
 const checkBtn = document.getElementById('checkButton');
 
 checkBtn.addEventListener('click', () => {
-    if (!input.value) return
+    if (!input.value) {
+        input.placeholder = 'Please type here';   
+    }
     
     let vowels = 'aeiouAEIOU';
     let valueArray = input.value.split('');
-
+    let counter = 0;
     for (let i = 0; i < valueArray.length; i++) {
-        if (valueArray[i] === vowels[i]) {
-            console.log(vowels[i]);
+        for (let j = 0; j < vowels.length; j++) {
+            if (valueArray[i] === vowels[j]) {
+                counter++;
+            }
         }
+        
     }
+    const h1 = document.createElement('h1');
+    
+    if (counter === 0) return 'no vowels found';
+    h1.innerText = `There are ${counter} ${counter > 1 ? 'vowels' : 'vowel'}!`;
+    document.body.append(h1);
+    clearInputField();
 })
+
+const clearInputField = () => { 
+    input.value = '';
+    
+}
